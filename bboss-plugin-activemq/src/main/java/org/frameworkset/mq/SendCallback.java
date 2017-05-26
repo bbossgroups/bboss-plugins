a8925c8aca99b3f2272868a19ae0656a26273ed0
yinbp <yin-bp@163.com>
2017-05-26 23:13:17 +0800

    更新插件版本号为5.0.3

--- bboss-plugin-activemq/src/main/java/org/frameworkset/mq/SendCallback.java
+++ bboss-plugin-activemq/src/main/java/org/frameworkset/mq/SendCallback.java
@@ -1,12 +1,13 @@
 package org.frameworkset.mq;
 
+import javax.jms.JMSException;
 import javax.jms.MessageProducer;
 
 /**
  * Created by 1 on 2017/5/26.
  */
 public interface SendCallback {
-    public abstract void sendMessage(MessageSession session,MessageProducer producer);
+    public abstract void sendMessage(MessageSession session,MessageProducer producer) throws JMSException;
     public boolean autocommit();
     /**
      * Session.AUTO_ACKNOWLEDGE
