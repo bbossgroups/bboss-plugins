e8a420864e204032c84f38d381b87818749c7376
yinbp <yin-bp@163.com>
2019-10-10 21:46:50 +0800

    修改测试用例

--- bboss-plugin-kafka/src/test/java/org/frameworkset/plugin/kafka/TestKafka.java
+++ bboss-plugin-kafka/src/test/java/org/frameworkset/plugin/kafka/TestKafka.java
@@ -1,15 +1,30 @@
 package org.frameworkset.plugin.kafka;
 
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 public class TestKafka {
 	public static void main(String[] args){
 		TestKafka testKafka = new TestKafka();
-		testKafka.testSend();
+		testKafka.testSend(false);
 	}
-	public void testSend() {
+	public void testSend(boolean syn) {
 		KafkaUtil kafkaUtil = new KafkaUtil("kafka_2.12-2.3.0/kafka.xml");
 		KafkaProductor productor = kafkaUtil.getProductor("kafkaproductor");
 		for (int i = 0; i < 100000; i++){
-			productor.send("blackcat", (long)i, "aaa" + i);
+			Future<RecordMetadata> recordMetadataFuture = productor.send("blackcat", (long)i, "aaa" + i);
+			if(syn) {
+				try {
+					RecordMetadata recordMetadata = recordMetadataFuture.get();
+					System.out.println(recordMetadata.topic() + "," + recordMetadata.offset() + "," + recordMetadata.partition());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
 			productor.send("blackcat", (long)i, "bbb" + i);
 			productor.send("blackcatbatchstore", (long)i, "aaa" + i);
 			productor.send("blackcatbatchstore", (long)i, "bbb" + i);
