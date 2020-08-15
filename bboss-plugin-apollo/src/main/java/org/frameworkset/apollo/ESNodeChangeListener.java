28c4fbfe5f3e89b1afb4ef93a577bd61e3594bea
yinbp <yin-bp@163.com>
2020-08-15 20:12:20 +0800

    添加动态切换是否打印dsl到控制台开关标识方法

--- bboss-plugin-apollo/src/main/java/org/frameworkset/apollo/ESNodeChangeListener.java
+++ bboss-plugin-apollo/src/main/java/org/frameworkset/apollo/ESNodeChangeListener.java
@@ -27,7 +27,11 @@ import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * <p>Description: </p>
+ * <p>Description:
+ * 监听es节点地址清单变化，并将变化更新到客户端es节点地址列表
+ * 监听集群路由规则是否变化，并切换到变化后的路由规则组
+ * 监听dsl打印控制开关变化，并更新客户端打印dsl控制开关
+ * </p>
  * <p></p>
  * <p>Copyright (c) 2020</p>
  * @Date 2020/8/9 23:10
@@ -38,16 +42,19 @@ public class ESNodeChangeListener extends PropertiesChangeListener {
 	private Set<String> elasticsearchPools;
 	private static Logger logger = LoggerFactory.getLogger(ESNodeChangeListener.class);
 	private static Method handleDiscoverHosts;
+	private static Method handleShowDsl;
 	static {
 		try {
 			Class clazz = Class.forName("org.frameworkset.elasticsearch.client.HostDiscoverUtil");
 			handleDiscoverHosts = clazz.getMethod("handleDiscoverHosts", String[].class, String.class);
+			handleShowDsl = clazz.getMethod("swithShowdsl", boolean.class, String.class);
 		}
 		catch (Exception e){
 
 		}
 	}
-	String defaultHostsKey = "elasticsearch.rest.hostNames";
+	private String defaultHostsKey = "elasticsearch.rest.hostNames";
+	private String defaultShowDslKey = "elasticsearch.showTemplate";
 	private void handleDiscoverHosts(String _hosts,String poolName){
 		if(_hosts != null && !_hosts.equals("")){
 			String[] hosts = _hosts.split(",");
@@ -64,17 +71,37 @@ public class ESNodeChangeListener extends PropertiesChangeListener {
 			}
 		}
 	}
+
+	private void handleShowDsl(String showDsl,String poolName){
+		if(showDsl != null && !showDsl.equals("")){
+
+			//将被动获取到的地址清单加入服务地址组poolName中
+			if(handleShowDsl != null) {
+				try {
+					handleShowDsl.invoke(null,showDsl.trim().equals("true"),poolName);
+				} catch (IllegalAccessException e) {
+					logger.error("handleShowDsl failed:showDsl["+showDsl+"],pool["+poolName+"]",e);
+				} catch (InvocationTargetException e) {
+					logger.error("handleShowDsl failed:showDsl["+showDsl+"],pool["+poolName+"]",e.getTargetException());
+				}
+			}
+		}
+	}
 	private void poolChange(ConfigChangeEvent changeEvent ,String pool){
 		Set<String> changedKeys = changeEvent.changedKeys();
 		ConfigChange hostsChange = null;
 		boolean isdefault = pool == null || pool.equals("default");
 		String hostsKey = null;
+		String showDslKey = null;
+		ConfigChange showDslChange = null;
 
 		if(!isdefault){
 			hostsKey = pool+".elasticsearch.rest.hostNames";
+			showDslKey = pool + ".elasticsearch.showTemplate";
 		}
 		else{
 			hostsKey = "default.elasticsearch.rest.hostNames";
+			showDslKey = "default.elasticsearch.showTemplate";
 		}
 
 		for (String key : changedKeys) {
@@ -83,6 +110,11 @@ public class ESNodeChangeListener extends PropertiesChangeListener {
 
 
 			}
+			if(key.equals(showDslKey) || (isdefault && key.equals(defaultShowDslKey))){//schedule集群
+				showDslChange = changeEvent.getChange(key);
+			}
+
+
 
 
 
@@ -93,8 +125,18 @@ public class ESNodeChangeListener extends PropertiesChangeListener {
 					hostsChange.getNewValue(), hostsChange.getChangeType());
 
 			String _hosts = hostsChange.getNewValue();
-			//连通host和rounting一同更新
+			//更新hosts
 			handleDiscoverHosts(_hosts, pool);
+
+		}
+		if(showDslChange != null && showDslChange.getChangeType() == PropertyChangeType.MODIFIED){
+			logger.info("Found change - key: {}, oldValue: {}, newValue: {}, changeType: {}",
+					showDslChange.getPropertyName(), showDslChange.getOldValue(),
+					showDslChange.getNewValue(), showDslChange.getChangeType());
+
+			String _showDsl = showDslChange.getNewValue();
+			//更新showdsl
+			handleShowDsl(_showDsl, pool);
 
 		}
 
