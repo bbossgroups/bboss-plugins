25d65e5f55fcc6c9e0b028370ea64fa83749bb98
大河 <yin-bp@163.com>
2025-03-20 11:37:53 +0800

    修复串行执行任务时报空指针问题："taskMetrics" is null

--- bboss-jodconverter-core/src/bboss/org/artofsolving/jodconverter/process/LinuxProcessManager.java
+++ bboss-jodconverter-core/src/bboss/org/artofsolving/jodconverter/process/LinuxProcessManager.java
@@ -31,7 +31,6 @@ import org.apache.commons.io.IOUtils;
  * <p>
  * Should Work on Solaris too, except that the command line string
  * returned by <tt>ps</tt> there is limited to 80 characters and this affects
- * {@link #findPid(String)}.
  */
 public class LinuxProcessManager implements ProcessManager {
 
